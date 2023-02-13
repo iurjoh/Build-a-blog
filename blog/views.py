d5fd8472c608354826d34aa904186d5ada5f0f64
Iuri Johansson <iuri_abreu@hotmail.com>
2023-02-13 15:49:12 +0000

    Add django bootstrap icons to use in share buttons

--- blog/views.py
+++ blog/views.py
@@ -7,7 +7,7 @@ from .forms import CommentForm
 
 class PostList(generic.ListView):
     model = Post
-    queryset = Post.objects.filter(status=1).order_by("-created_on")
+    queryset = Post.objects.filter(status=1).order_by("created_on")
     template_name = "index.html"
     paginate_by = 6
 
